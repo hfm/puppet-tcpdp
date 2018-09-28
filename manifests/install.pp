cee6c300ed32ef0700af3329c59604cbcaeb9ff5
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-09-28 20:02:26 +0900

    installation

---
+++ manifests/install.pp
@@ -0,0 +1,73 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include tcpdp::install
+class tcpdp::install {
+
+  include ::archive
+
+  $_path = "${$tcpdp::install_dir}/tcpdp-${tcpdp::version}"
+
+  # URL Examples:
+  # * https://github.com/k1LoW/tcpdp/releases/download/v0.9.1/tcpdp_v0.9.1_linux_amd64.el7.tar.gz
+  # * https://github.com/k1LoW/tcpdp/releases/download/v0.9.1/tcpdp_v0.9.1_linux_amd64_static.el7.tar.gz
+  # * https://github.com/k1LoW/tcpdp/releases/download/v0.9.1/tcpdp_v0.9.1_linux_amd64.xenial.tar.gz
+  # * https://github.com/k1LoW/tcpdp/releases/download/v0.9.1/tcpdp_v0.9.1_linux_amd64_static.xenial.tar.gz
+  case $facts['kernel'] {
+    'Darwin': {
+      $_url = "${tcpdp::download_url_base}_darwin_amd64.zip"
+    }
+    'Linux': {
+      $_url = $tcpdp::use_static ? {
+        true  => "${tcpdp::download_url_base}_linux_amd64_static",
+        false => "${tcpdp::download_url_base}_linux_amd64",
+      }
+
+      $_url = $facts['os']['family'] ? {
+        'RedHat' => "${_url}.el${facts['os']['release']['major']}.tar.gz",
+        'Debian' => "${_url}.${facts['os']['distro']['codename']}.tar.gz",
+      }
+    }
+    default: {
+      fail("Unsupported kernel: ${facts['kernel']}")
+    }
+  }
+
+  file { [
+    $tcpdp::install_dir,
+    $_path,
+  ]:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0555',
+    before => Archive['tcpdp'],
+  }
+
+
+  archive { 'tcpdp':
+    ensure       => present,
+    source       => $_url,
+    extract      => true,
+    extract_path => $_path,
+    creates      => "${_path}/tcpdp",
+  }
+
+  file {
+    default:
+      require => Archive['tcpdp'];
+
+    "${_path}/tcpdp":
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0700';
+
+    "${tcpdp::bin_dir}/tcpdp":
+      ensure => link,
+      target => "${_path}/tcpdp";
+  }
+
+}
---
+++ spec/acceptance/tcpdp_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper_acceptance'
+
+describe 'tcpdp class' do
+  let(:manifest) do
+    <<-EOS
+      include ::tcpdp
+    EOS
+  end
+
+  it 'runs without errors' do
+    expect(apply_manifest(manifest, catch_failures: true).exit_code).to eq 2
+  end
+
+  it 'runs a second time without changes' do
+    expect(apply_manifest(manifest).exit_code).to be_zero
+  end
+
+  ['/opt/tcpdp', '/opt/tcpdp/tcpdp-0.9.1'].each do |dir|
+    describe file(dir) do
+      it { is_expected.to be_directory }
+      it { is_expected.to be_owned_by 'root' }
+      it { is_expected.to be_grouped_into 'root' }
+      it { is_expected.to be_mode 555 }
+    end
+  end
+
+  describe file('/opt/tcpdp/tcpdp-0.9.1/tcpdp') do
+    it { is_expected.to be_executable }
+    it { is_expected.to be_owned_by 'root' }
+    it { is_expected.to be_grouped_into 'root' }
+  end
+
+  describe file('/usr/local/bin/tcpdp') do
+    it { is_expected.to be_symlink }
+    its(:link_target) { is_expected.to be '/opt/tcpdp/tcpdp-0.9.1/tcpdp' }
+  end
+end
