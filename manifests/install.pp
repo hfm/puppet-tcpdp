5a8465efc0bcb1e2c0ba0c36cf61128a29838ec0
OKUMURA Takahiro <hfm.garden@gmail.com>
2018-09-28 20:22:23 +0900

    archive path must be absolute

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,7 +8,7 @@ class tcpdp::install {
 
   include ::archive
 
-  $_path = "${$tcpdp::install_dir}/tcpdp-${tcpdp::version}"
+  $_path = "${tcpdp::install_dir}/tcpdp-${tcpdp::version}"
 
   # URL Examples:
   # * https://github.com/k1LoW/tcpdp/releases/download/v0.9.1/tcpdp_v0.9.1_linux_amd64.el7.tar.gz
@@ -17,17 +17,20 @@ class tcpdp::install {
   # * https://github.com/k1LoW/tcpdp/releases/download/v0.9.1/tcpdp_v0.9.1_linux_amd64_static.xenial.tar.gz
   case $facts['kernel'] {
     'Darwin': {
-      $_url = "${tcpdp::download_url_base}_darwin_amd64.zip"
+      $_ext = "zip"
+      $_url = "${tcpdp::download_url_base}_darwin_amd64.${_ext}"
     }
     'Linux': {
+      $_ext = "tar.gz"
+
       $_url_pre = $tcpdp::use_static ? {
         true  => "${tcpdp::download_url_base}_linux_amd64_static",
         false => "${tcpdp::download_url_base}_linux_amd64",
       }
 
       $_url = $facts['os']['family'] ? {
-        'RedHat' => "${_url_pre}.el${facts['os']['release']['major']}.tar.gz",
-        'Debian' => "${_url_pre}.${facts['os']['distro']['codename']}.tar.gz",
+        'RedHat' => "${_url_pre}.el${facts['os']['release']['major']}.${_ext}",
+        'Debian' => "${_url_pre}.${facts['os']['distro']['codename']}.${_ext}",
       }
     }
     default: {
@@ -43,11 +46,11 @@ class tcpdp::install {
     owner  => 'root',
     group  => 'root',
     mode   => '0555',
-    before => Archive['tcpdp'],
+    before => Archive["${_path}.${_ext}"],
   }
 
 
-  archive { 'tcpdp':
+  archive { "${_path}.${_ext}":
     ensure       => present,
     source       => $_url,
     extract      => true,
@@ -57,7 +60,7 @@ class tcpdp::install {
 
   file {
     default:
-      require => Archive['tcpdp'];
+      require => Archive["${_path}.${_ext}"];
 
     "${_path}/tcpdp":
       ensure => file,
